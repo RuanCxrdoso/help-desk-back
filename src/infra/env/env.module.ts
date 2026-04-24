a6d9aaba39a2fedc8b826abd62037e9fdfb0a20b
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-04-24 19:48:33 -0300

    :sparkles: feat: Implement environment configuration module and service with validation schema

---
+++ src/infra/env/env.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
+import { EnvService } from './env.service'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (env) => envSchema.parse(env),
+    }),
+  ],
+  providers: [EnvService],
+  exports: [EnvService],
+})
+export class EnvModule {}
