36c8e5e7f9d30f174eab9d40716b09bc634ff978
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-05-10 19:17:28 -0300

    :sparkles: feat: Add PrismaAdminsRepository for managing Admin entities

--- src/infra/database/prisma/prisma.service.ts
+++ src/infra/database/prisma/prisma.service.ts
@@ -1,4 +1,3 @@
-import 'dotenv/config'
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from 'generated/prisma/client'
---
+++ src/infra/database/prisma/repositories/admins-repository.ts
@@ -0,0 +1,33 @@
+import { IAdminsRepository } from '@/domain/help-desk/application/repositories/admins-repository'
+import { Admin } from '@/domain/help-desk/enterprise/entities/admin'
+import { PrismaService } from '../prisma.service'
+import { Injectable } from '@nestjs/common'
+import { AdminMapper } from '../mappers/admin-mapper'
+
+@Injectable()
+export class PrismaAdminsRepository implements IAdminsRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(user: Admin): Promise<void> {
+    const prismaAdmin = AdminMapper.toPrisma(user)
+
+    await this.prisma.user.create({
+      data: prismaAdmin,
+    })
+  }
+
+  async findById(id: string, tenantId: string): Promise<Admin | null> {
+    const admin = await this.prisma.user.findFirst({
+      where: {
+        id,
+        tenantId,
+      },
+    })
+
+    if (!admin) return null
+
+    const domainAdmin = AdminMapper.toDomain(admin)
+
+    return domainAdmin
+  }
+}
