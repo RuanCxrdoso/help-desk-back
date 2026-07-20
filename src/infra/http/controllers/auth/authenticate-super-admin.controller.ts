4e1452060a1d1c9f66d93dfb11145dc566340448
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-07-20 16:59:41 -0300

    :sparkles: feat: Add AuthenticateSuperAdminController for super admin login with validation and error handling

---
+++ src/infra/http/controllers/auth/authenticate-super-admin.controller.ts
@@ -0,0 +1,65 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UnauthorizedException,
+} from '@nestjs/common'
+import { Public } from '../../auth/public.decorator'
+import z from 'zod'
+import { ZodValidationPipe } from '../../utils/pipes/zod-validation.pipe'
+import { AuthenticateSuperAdminUseCase } from '@/domain/help-desk/application/use-cases/authenticate-super-admin'
+import { InvalidCredentialsError } from '@/domain/help-desk/application/errors/invalid-credentials-error'
+
+const authenticateSuperAdminSchema = z.object({
+  email: z.email().min(1, { error: 'E-mail must be provided.' }),
+  password: z
+    .string()
+    .min(6, { error: 'Password must have at least 6 characters.' }),
+})
+
+type AuthenticateSuperAdminBodyType = z.infer<
+  typeof authenticateSuperAdminSchema
+>
+
+const authenticateSuperAdminBodyPipe = new ZodValidationPipe(
+  authenticateSuperAdminSchema,
+)
+
+@Controller('/auth')
+export class AuthenticateSuperAdminController {
+  constructor(
+    private readonly authenticateSuperAdminUseCase: AuthenticateSuperAdminUseCase,
+  ) {}
+
+  @Post('/super-admin/login')
+  @HttpCode(200)
+  @Public()
+  async handle(
+    @Body(authenticateSuperAdminBodyPipe) body: AuthenticateSuperAdminBodyType,
+  ) {
+    const { email, password } = body
+
+    const result = await this.authenticateSuperAdminUseCase.execute({
+      email,
+      password,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+
+      switch (error.constructor) {
+        case InvalidCredentialsError:
+          throw new UnauthorizedException('Invalid credentials.')
+        default:
+          throw new UnauthorizedException(error.message)
+      }
+    }
+
+    const accessToken = result.value.accessToken
+
+    return {
+      access_token: accessToken,
+    }
+  }
+}
