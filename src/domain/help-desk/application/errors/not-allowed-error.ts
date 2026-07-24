1d8759cf70ef9d7d89ff3639f4fa99ce37cbd2db
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-07-24 16:27:29 -0300

    :sparkles: feat: Update NotAllowedError constructor to accept custom message

--- src/domain/help-desk/application/errors/not-allowed-error.ts
+++ src/domain/help-desk/application/errors/not-allowed-error.ts
@@ -2,7 +2,7 @@ import { DomainError } from '@/core/error/domain-error'
 import { IUseCaseError } from '@/core/error/use-case-error'
 
 export class NotAllowedError extends DomainError implements IUseCaseError {
-  constructor() {
-    super('Not Allowed.')
+  constructor(message = 'Not Allowed.') {
+    super(message)
   }
 }
