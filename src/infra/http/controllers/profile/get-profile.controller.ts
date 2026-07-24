f0bd53c149221a31ed919e1108c1fa97c7bad720
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-07-24 11:37:48 -0300

    :sparkles: feat: Implement GetProfileController for user profile retrieval with role-based handling

---
+++ src/infra/http/controllers/profile/get-profile.controller.ts
@@ -0,0 +1,84 @@
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common'
+import { GetAdminProfileUseCase } from '@/domain/help-desk/application/use-cases/get-admin-profile'
+import { GetEmployeeProfileUseCase } from '@/domain/help-desk/application/use-cases/get-employee-profile'
+import { GetTechnicianProfileUseCase } from '@/domain/help-desk/application/use-cases/get-technician-profile'
+import { PoliciesGuard } from '../../auth/casl/policies.guard'
+import { CheckPolicies } from '../../auth/casl/check-policies.decorator'
+import { User } from '../../utils/decorators/user.decorator'
+import { type TokenPayload } from '../../auth/jwt.strategy'
+import { ROLE } from '@/core/enums/role'
+import { Action } from '../../auth/casl/action'
+import {
+  HttpAdminProfilePresenter,
+  HttpEmployeeProfilePresenter,
+  HttpTechnicianProfilePresenter,
+} from '@/infra/presenters/http-profile.presenter'
+import { NotAllowedError } from '@/domain/help-desk/application/errors/not-allowed-error'
+
+@Controller()
+export class GetProfileController {
+  constructor(
+    private readonly getAdminProfileUseCase: GetAdminProfileUseCase,
+    private readonly getTechnicianProfileUseCase: GetTechnicianProfileUseCase,
+    private readonly getEmployeeProfileUseCase: GetEmployeeProfileUseCase,
+  ) {}
+
+  @Get('/me')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability) => ability.can(Action.Read, 'User'))
+  async handle(@User() user: TokenPayload) {
+    const { sub: id, tenantId, role } = user
+
+    switch (role) {
+      case ROLE.ADMIN: {
+        const result = await this.getAdminProfileUseCase.execute({
+          id,
+          tenantId,
+          role,
+        })
+
+        if (result.isLeft()) throw result.value
+
+        const adminDomain = result.value.admin
+
+        return HttpAdminProfilePresenter.toHttp(adminDomain)
+      }
+      case ROLE.TECHNICIAN: {
+        const result = await this.getTechnicianProfileUseCase.execute({
+          id,
+          tenantId,
+          role,
+        })
+
+        if (result.isLeft()) throw result.value
+
+        const technicianDomain = result.value.technician
+
+        return HttpTechnicianProfilePresenter.toHttp(technicianDomain)
+      }
+      case ROLE.EMPLOYEE: {
+        const result = await this.getEmployeeProfileUseCase.execute({
+          id,
+          tenantId,
+          role,
+        })
+
+        if (result.isLeft()) throw result.value
+
+        const employeeDomain = result.value.employee
+
+        return HttpEmployeeProfilePresenter.toHttp(employeeDomain)
+      }
+      default: {
+        throw new NotAllowedError()
+      }
+    }
+  }
+}
