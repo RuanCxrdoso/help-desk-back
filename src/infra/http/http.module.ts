a01260544ebca577356b4217d3c3e126bd190aec
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-07-28 11:01:35 -0300

    :bug: fix: Add missing controllers and use case to HttpModule

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -6,16 +6,32 @@ import { RegisterEmployeeUseCase } from '@/domain/help-desk/application/use-case
 import { APP_FILTER } from '@nestjs/core'
 import { DomainErrorFilter } from './filters/domain-error.filter'
 import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './controllers/auth/authenticate.controller'
+import { AuthenticateUseCase } from '@/domain/help-desk/application/use-cases/authenticate'
+import { AuthenticateSuperAdminController } from './controllers/auth/authenticate-super-admin.controller'
+import { AdminRegisterController } from './controllers/register/admin-register.controller'
+import { TechnicianRegisterController } from './controllers/register/technician-register.controller'
+import { TenantAndAdminRegisterController } from './controllers/register/tenant-and-admin-register.controller'
+import { GetProfileController } from './controllers/profile/get-profile.controller'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule, AuthModule],
   providers: [
     RegisterEmployeeUseCase,
+    AuthenticateUseCase,
     {
       provide: APP_FILTER,
       useClass: DomainErrorFilter,
     },
   ],
-  controllers: [EmployeeRegisterController],
+  controllers: [
+    AuthenticateSuperAdminController,
+    AuthenticateController,
+    TenantAndAdminRegisterController,
+    AdminRegisterController,
+    TechnicianRegisterController,
+    EmployeeRegisterController,
+    GetProfileController,
+  ],
 })
 export class HttpModule {}
