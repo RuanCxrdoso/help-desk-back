df382656f0276795569fa1b6903ce8a4680eeaa6
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-07-30 16:45:59 -0300

    :sparkles: feat: Implement FetchEmployeesUseCase for retrieving employee data with pagination support

---
+++ src/domain/help-desk/application/use-cases/fetch-employees.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common'
+import { IEmployeesRepository } from '../repositories/employees-repository'
+import { Either, right } from '@/core/error/either'
+import { AuthUser } from '../../enterprise/entities/auth-user'
+
+interface FetchEmployeesUseCaseRequest {
+  tenantId: string
+  params: {
+    q: string | null
+    page: number
+    perPage: number
+    orderBy: string
+    order: 'asc' | 'desc'
+  }
+}
+
+interface FetchEmployeesRightResponse {
+  employees: AuthUser[]
+  meta: {
+    page: number
+    perPage: number
+    totalCount: number
+    totalPages: number
+    orderBy: string
+    order: 'asc' | 'desc'
+  }
+}
+
+type FetchEmployeesUseCaseReponse = Either<null, FetchEmployeesRightResponse>
+
+@Injectable()
+export class FetchEmployeesUseCase {
+  constructor(private readonly employeesRepository: IEmployeesRepository) {}
+
+  async execute({
+    tenantId,
+    params,
+  }: FetchEmployeesUseCaseRequest): Promise<FetchEmployeesUseCaseReponse> {
+    const { items, page, perPage, totalCount, totalPages, orderBy, order } =
+      await this.employeesRepository.findMany(tenantId, params)
+
+    return right({
+      employees: items,
+      meta: {
+        page,
+        perPage,
+        totalCount,
+        totalPages,
+        orderBy,
+        order,
+      },
+    })
+  }
+}
