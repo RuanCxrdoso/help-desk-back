c76d133b22711373c2d2546461a59b6d9a91ff21
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-08-04 10:46:29 -0300

    :art: feat: Implement temporary findMany method in Admin, Employee, and Technician repositories

--- src/infra/database/prisma/repositories/technicians-repository.ts
+++ src/infra/database/prisma/repositories/technicians-repository.ts
@@ -4,10 +4,23 @@ import { Technician } from '@/domain/help-desk/enterprise/entities/technician'
 import { TechnicianMapper } from '../mappers/technician-mapper'
 import { Injectable } from '@nestjs/common'
 import { ROLE } from 'generated/prisma/enums'
+import { PaginationParams, PaginatedResult } from '@/core/types/pagination'
+import { AuthUser } from '@/domain/help-desk/enterprise/entities/auth-user'
 
 @Injectable()
 export class PrismaTechniciansRepository implements ITechniciansRepository {
   constructor(private readonly prisma: PrismaService) {}
+  findMany(
+    tenantId: string,
+    params: PaginationParams,
+  ): Promise<PaginatedResult<AuthUser>> {
+    console.log(
+      '🚀 ~ PrismaTechniciansRepository ~ findMany ~ tenantId:',
+      tenantId,
+    )
+    console.log('🚀 ~ PrismaTechniciansRepository ~ findMany ~ params:', params)
+    throw new Error('Method not implemented.')
+  }
 
   async create(user: Technician): Promise<void> {
     const technicianPrisma = TechnicianMapper.toPrismaUser(user)
