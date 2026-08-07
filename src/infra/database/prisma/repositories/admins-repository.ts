7a1d24a272005fe46192fa518a05ded4470d7459
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-08-07 16:43:57 -0300

    ✨ feat: implement findMany in PrismaAdminsRepository

--- src/infra/database/prisma/repositories/admins-repository.ts
+++ src/infra/database/prisma/repositories/admins-repository.ts
@@ -6,6 +6,8 @@ import { AdminMapper } from '../mappers/admin-mapper'
 import { ROLE } from 'generated/prisma/enums'
 import { PaginationParams, PaginatedResult } from '@/core/types/pagination'
 import { AuthUser } from '@/domain/help-desk/enterprise/entities/auth-user'
+import { UserWhereInput } from 'generated/prisma/models'
+import { AuthUserMapper } from '../mappers/auth-user-mapper'
 
 @Injectable()
 export class PrismaAdminsRepository implements IAdminsRepository {
@@ -40,12 +42,60 @@ export class PrismaAdminsRepository implements IAdminsRepository {
     tenantId: string,
     params: PaginationParams,
   ): Promise<PaginatedResult<AuthUser>> {
-    console.log(
-      '🚀 ~ PrismaTechniciansRepository ~ findMany ~ tenantId:',
+    const {
+      q,
+      page,
+      perPage,
+      order = 'asc',
+      orderBy = 'firstName',
+      status,
+    } = params
+
+    const skip = (page - 1) * perPage
+    const take = perPage
+
+    let isActive: boolean | undefined = undefined
+    if (status === 'ACTIVE') {
+      isActive = true
+    } else if (status === 'INACTIVE') {
+      isActive = false
+    }
+    const whereClause: UserWhereInput = {
       tenantId,
-    )
-    console.log('🚀 ~ PrismaTechniciansRepository ~ findMany ~ params:', params)
-    throw new Error('Method not implemented.')
+      role: ROLE.ADMIN,
+      isActive,
+      ...(q && {
+        OR: [
+          { firstName: { contains: q, mode: 'insensitive' } },
+          { lastName: { contains: q, mode: 'insensitive' } },
+          { email: { contains: q, mode: 'insensitive' } },
+        ],
+      }),
+    }
+
+    const [totalCount, items] = await this.prisma.$transaction([
+      this.prisma.user.count({ where: whereClause }),
+      this.prisma.user.findMany({
+        where: whereClause,
+        take,
+        skip,
+        orderBy: {
+          [orderBy]: order,
+        },
+      }),
+    ])
+
+    return {
+      items: items.map(AuthUserMapper.toDomain),
+      meta: {
+        page,
+        perPage,
+        totalCount,
+        totalPages: Math.ceil(totalCount / perPage),
+        orderBy,
+        order,
+      },
+    }
   }
 
   async save(admin: Admin): Promise<void> {
