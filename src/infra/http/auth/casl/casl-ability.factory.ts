43db8b3af1cdfbd3f252838ab05140ee7144bfa9
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-08-09 17:43:24 -0300

    ✨ feat: Enhance ADMIN role permissions for User management in CaslAbilityFactory

--- src/infra/http/auth/casl/casl-ability.factory.ts
+++ src/infra/http/auth/casl/casl-ability.factory.ts
@@ -40,7 +40,17 @@ export class CaslAbilityFactory {
     }
 
     if (user.role === ROLE.ADMIN) {
-      can(Manage, 'User', { tenantId: user.tenantId })
+      can(Create, 'User', { tenantId: user.tenantId })
+      can(Read, 'User', { tenantId: user.tenantId })
+      can(Update, 'User', { tenantId: user.tenantId, id: user.sub })
+      can(Update, 'User', {
+        tenantId: user.tenantId,
+        role: { $in: [ROLE.EMPLOYEE, ROLE.TECHNICIAN] },
+      })
+      can(Delete, 'User', {
+        tenantId: user.tenantId,
+        role: { $in: [ROLE.EMPLOYEE, ROLE.TECHNICIAN] },
+      })
 
       can(Read, 'Tenant', { id: user.tenantId })
       can(Update, 'Tenant', { id: user.tenantId })
