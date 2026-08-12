e45ebf28c7287ec05eec051113b3bfda56362d8f
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-08-12 17:03:51 -0300

    :test_tube: test: Integrate callerPayload into GetAdminProfileUseCase tests for enhanced authorization checks

--- src/domain/help-desk/application/use-cases/tests/get-admin-profile.spec.ts
+++ src/domain/help-desk/application/use-cases/tests/get-admin-profile.spec.ts
@@ -4,16 +4,21 @@ import { GetAdminProfileUseCase } from '../get-admin-profile'
 import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repository'
 import { NotFoundError } from '../../errors/not-found-error'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { FakeAuthorizationService } from 'test/services/authorization-service'
+import { ROLE } from '@/core/enums/role'
+import { NotAllowedError } from '../../errors/not-allowed-error'
 
+let authorizationService: FakeAuthorizationService
 let usersRepository: InMemoryUsersRepository
 let adminsRepository: InMemoryAdminsRepository
 let sut: GetAdminProfileUseCase
 
 describe('Get Admin Profile', () => {
   beforeEach(() => {
+    authorizationService = new FakeAuthorizationService()
     usersRepository = new InMemoryUsersRepository()
     adminsRepository = new InMemoryAdminsRepository(usersRepository)
-    sut = new GetAdminProfileUseCase(adminsRepository)
+    sut = new GetAdminProfileUseCase(adminsRepository, authorizationService)
   })
 
   it('should be able to get their profile', async () => {
@@ -26,6 +31,11 @@ describe('Get Admin Profile', () => {
     const result = await sut.execute({
       id: admin.id.toString(),
       tenantId: admin.tenantId.toString(),
+      callerPayload: {
+        sub: 'user-1',
+        tenantId: 'tenant-1',
+        role: ROLE.ADMIN,
+      },
     })
     expect(result.isRight()).toBeTruthy()
     expect(result.value).toEqual({
@@ -33,12 +43,28 @@ describe('Get Admin Profile', () => {
         firstName: 'Lewis Hamilton',
       }),
     })
+
+    expect(authorizationService.authorizeSpy).toHaveBeenCalledWith(
+      {
+        sub: 'user-1',
+        tenantId: 'tenant-1',
+        role: ROLE.ADMIN,
+      },
+      'read',
+      'User',
+      expect.anything(),
+    )
   })
 
   it('should not be able to get an inexistent employee profile', async () => {
     const result = await sut.execute({
       id: 'invalid-id',
       tenantId: 'tenant-1',
+      callerPayload: {
+        sub: 'user-1',
+        tenantId: 'tenant-1',
+        role: ROLE.ADMIN,
+      },
     })
 
     expect(result.isLeft()).toBeTruthy()
@@ -55,9 +81,38 @@ describe('Get Admin Profile', () => {
     const result = await sut.execute({
       id: employee.id.toString(),
       tenantId: 'tenant-2', // Simula a requisição originada de um tenant diferente
+      callerPayload: {
+        sub: 'user-1',
+        tenantId: 'tenant-1',
+        role: ROLE.ADMIN,
+      },
     })
 
     expect(result.isLeft()).toBeTruthy()
     expect(result.value).toBeInstanceOf(NotFoundError)
+  })
+
+  it('should not be able to get a profile if not authorized', async () => {
+    const admin = makeAdmin(
+      { tenantId: new UniqueEntityID('tenant-1') },
+      new UniqueEntityID('admin-1'),
+    )
+
+    adminsRepository.items.push(admin)
+
+    authorizationService.mockAuthorization(false)
+
+    const result = await sut.execute({
+      id: 'admin-1',
+      tenantId: 'tenant-1',
+      callerPayload: {
+        sub: 'user-1',
+        tenantId: 'tenant-1',
+        role: ROLE.ADMIN,
+      },
+    })
+
+    expect(result.isLeft()).toBeTruthy()
+    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
