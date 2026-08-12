86322c062dc384617a608585458005f2567e919e
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-08-12 14:39:51 -0300

    ✨ feat: Implement DeleteTechnicianController to manage technician deletion requests with authorization checks

--- src/domain/help-desk/application/use-cases/delete-technician.ts
+++ src/domain/help-desk/application/use-cases/delete-technician.ts
@@ -2,26 +2,48 @@ import { Either, right, left } from '@/core/error/either'
 import { ITechniciansRepository } from '../repositories/technicians-repository'
 import { Injectable } from '@nestjs/common'
 import { NotFoundError } from '../errors/not-found-error'
+import { TokenPayload } from '@/infra/http/auth/jwt.strategy'
+import { NotAllowedError } from '../errors/not-allowed-error'
+import { IAuthorizationService } from '../auth/authorization.service'
 
 export interface DeleteTechnicianUseCaseRequest {
   id: string
   tenantId: string
+  callerPayload: TokenPayload
 }
 
-type DeleteTechnicianUseCaseResponse = Either<NotFoundError, null>
+type DeleteTechnicianUseCaseResponse = Either<
+  NotFoundError | NotAllowedError,
+  null
+>
 
 @Injectable()
 export class DeleteTechnicianUseCase {
-  constructor(private readonly techniciansRepository: ITechniciansRepository) {}
+  constructor(
+    private readonly techniciansRepository: ITechniciansRepository,
+    private readonly authorizationService: IAuthorizationService,
+  ) {}
 
   async execute({
     id,
     tenantId,
+    callerPayload,
   }: DeleteTechnicianUseCaseRequest): Promise<DeleteTechnicianUseCaseResponse> {
     const technician = await this.techniciansRepository.findById(id, tenantId)
 
     if (!technician) {
       return left(new NotFoundError())
+    }
+
+    if (
+      !this.authorizationService.authorize(
+        callerPayload,
+        'delete',
+        'User',
+        technician,
+      )
+    ) {
+      return left(new NotAllowedError())
     }
 
     technician.deactivate()
