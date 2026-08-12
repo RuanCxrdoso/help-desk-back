729cd7f5ba1033c4140b3ae4b54689457ad9ace9
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-08-12 17:13:24 -0300

    :test_tube: test: Integrate callerPayload into GetEmployeeProfileUseCase for enhanced authorization checks

--- src/domain/help-desk/application/use-cases/tests/get-employee-profile.spec.ts
+++ src/domain/help-desk/application/use-cases/tests/get-employee-profile.spec.ts
@@ -4,16 +4,24 @@ import { GetEmployeeProfileUseCase } from '../get-employee-profile'
 import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repository'
 import { NotFoundError } from '../../errors/not-found-error'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { FakeAuthorizationService } from 'test/services/authorization-service'
+import { ROLE } from '@/core/enums/role'
+import { NotAllowedError } from '../../errors/not-allowed-error'
 
+let authorizationService: FakeAuthorizationService
 let usersRepository: InMemoryUsersRepository
 let employeesRepository: InMemoryEmployeesRepository
 let sut: GetEmployeeProfileUseCase
 
 describe('Get Employee Profile', () => {
   beforeEach(() => {
+    authorizationService = new FakeAuthorizationService()
     usersRepository = new InMemoryUsersRepository()
     employeesRepository = new InMemoryEmployeesRepository(usersRepository)
-    sut = new GetEmployeeProfileUseCase(employeesRepository)
+    sut = new GetEmployeeProfileUseCase(
+      employeesRepository,
+      authorizationService,
+    )
   })
 
   it('should be able to get their profile', async () => {
@@ -26,6 +34,11 @@ describe('Get Employee Profile', () => {
     const result = await sut.execute({
       id: employee.id.toString(),
       tenantId: employee.tenantId.toString(),
+      callerPayload: {
+        sub: employee.id.toString(),
+        role: employee.role,
+        tenantId: employee.tenantId.toString(),
+      },
     })
     expect(result.isRight()).toBeTruthy()
     expect(result.value).toEqual({
@@ -33,12 +46,28 @@ describe('Get Employee Profile', () => {
         firstName: 'Lebron James',
       }),
     })
+
+    expect(authorizationService.authorizeSpy).toHaveBeenCalledWith(
+      {
+        sub: employee.id.toString(),
+        role: employee.role,
+        tenantId: employee.tenantId.toString(),
+      },
+      'read',
+      'User',
+      expect.anything(),
+    )
   })
 
   it('should not be able to get an inexistent employee profile', async () => {
     const result = await sut.execute({
       id: 'invalid-id',
       tenantId: 'tenant-1',
+      callerPayload: {
+        sub: 'user-1',
+        tenantId: 'tenant-1',
+        role: ROLE.ADMIN,
+      },
     })
 
     expect(result.isLeft()).toBeTruthy()
@@ -55,9 +84,37 @@ describe('Get Employee Profile', () => {
     const result = await sut.execute({
       id: employee.id.toString(),
       tenantId: 'tenant-2',
+      callerPayload: {
+        sub: 'user-1',
+        tenantId: 'tenant-1',
+        role: ROLE.ADMIN,
+      },
     })
 
     expect(result.isLeft()).toBeTruthy()
     expect(result.value).toBeInstanceOf(NotFoundError)
+  })
+
+  it('should not be able to get a profile if the user is not authorized', async () => {
+    const employee = makeEmployee({
+      firstName: 'Lebron James',
+    })
+
+    employeesRepository.items.push(employee)
+
+    authorizationService.mockAuthorization(false)
+
+    const result = await sut.execute({
+      id: employee.id.toString(),
+      tenantId: employee.tenantId.toString(),
+      callerPayload: {
+        sub: 'user-1',
+        tenantId: employee.tenantId.toString(),
+        role: ROLE.ADMIN,
+      },
+    })
+
+    expect(result.isLeft()).toBeTruthy()
+    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
