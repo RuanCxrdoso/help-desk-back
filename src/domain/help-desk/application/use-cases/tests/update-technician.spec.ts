5c0cf80ce6ac5e40142637529c2935168a55acd0
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-08-13 16:43:03 -0300

    :test_tube: test: Integrate callerPayload and authorization checks into UpdateTechnicianUseCase tests

--- src/domain/help-desk/application/use-cases/tests/update-technician.spec.ts
+++ src/domain/help-desk/application/use-cases/tests/update-technician.spec.ts
@@ -4,16 +4,24 @@ import { UpdateTechnicianUseCase } from '../update-technician'
 import { makeTechnician } from 'test/factories/make-technician'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { NotFoundError } from '../../errors/not-found-error'
+import { FakeAuthorizationService } from 'test/services/authorization-service'
+import { ROLE } from '@/core/enums/role'
+import { NotAllowedError } from '../../errors/not-allowed-error'
 
+let authorizationService: FakeAuthorizationService
 let usersRepository: InMemoryUsersRepository
 let techniciansRepository: InMemoryTechniciansRepository
 let sut: UpdateTechnicianUseCase
 
 describe('Update Technician', () => {
   beforeEach(() => {
+    authorizationService = new FakeAuthorizationService()
     usersRepository = new InMemoryUsersRepository()
     techniciansRepository = new InMemoryTechniciansRepository(usersRepository)
-    sut = new UpdateTechnicianUseCase(techniciansRepository)
+    sut = new UpdateTechnicianUseCase(
+      techniciansRepository,
+      authorizationService,
+    )
 
     for (let i = 1; i <= 2; i++) {
       techniciansRepository.items.push(
@@ -39,6 +47,11 @@ describe('Update Technician', () => {
       lastName: 'updated-last-name',
       supportLevel: 3,
       specialties: ['updated-specialty-1'],
+      callerPayload: {
+        sub: 'user-id',
+        tenantId: 'tenant-id-1',
+        role: ROLE.ADMIN,
+      },
     })
 
     expect(result.isRight()).toBe(true)
@@ -61,6 +74,17 @@ describe('Update Technician', () => {
         }),
       )
     }
+
+    expect(authorizationService.authorizeSpy).toHaveBeenCalledWith(
+      {
+        sub: 'user-id',
+        tenantId: 'tenant-id-1',
+        role: ROLE.ADMIN,
+      },
+      'update',
+      'User',
+      expect.anything(),
+    )
   })
 
   it('should not be able to update a non-existent technician', async () => {
@@ -71,6 +95,11 @@ describe('Update Technician', () => {
       lastName: 'updated-last-name',
       supportLevel: 3,
       specialties: ['updated-specialty-1'],
+      callerPayload: {
+        sub: 'user-id',
+        tenantId: 'tenant-id-3',
+        role: ROLE.ADMIN,
+      },
     })
 
     expect(result.isLeft()).toBe(true)
@@ -88,6 +117,11 @@ describe('Update Technician', () => {
       lastName: 'lastName-1',
       supportLevel: 1,
       specialties: ['specialty-1'],
+      callerPayload: {
+        sub: 'user-id',
+        tenantId: 'tenant-id-1',
+        role: ROLE.ADMIN,
+      },
     })
 
     expect(result.isRight()).toBe(true)
@@ -102,5 +136,36 @@ describe('Update Technician', () => {
         }),
       )
     }
+  })
+
+  it('should not be able to update a technician if not authorized', async () => {
+    authorizationService.mockAuthorization(false)
+
+    const result = await sut.execute({
+      id: 'id-1',
+      tenantId: 'tenant-id-1',
+      firstName: 'updated-first-name',
+      lastName: 'updated-last-name',
+      supportLevel: 3,
+      specialties: ['updated-specialty-1'],
+      callerPayload: {
+        sub: 'user-id',
+        tenantId: 'tenant-id-1',
+        role: ROLE.ADMIN,
+      },
+    })
+
+    expect(result.isLeft()).toBe(true)
+
+    if (result.isLeft()) {
+      expect(result.value).toBeInstanceOf(NotAllowedError)
+    }
+
+    expect(techniciansRepository.items[0]).toEqual(
+      expect.objectContaining({
+        firstName: 'Technician-1',
+        lastName: 'lastName-1',
+      }),
+    )
   })
 })
